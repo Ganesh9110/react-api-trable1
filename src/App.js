d7a2f4e922b6ac6bec47febbae4ed9c28b99de47
Ganesh <kolluriganesh777@email.com>
2025-09-26 13:36:06 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -2,20 +2,30 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Posts from "./Pages/Post";
 
 function App() {
-    return (
-        <Router>
-            <div>
-                <nav style={{ padding: "20px", textAlign: "center", background: "#333" }}>
-                    <Link to="/" style={{ color: "#fff", textDecoration: "none", fontWeight: "bold" }}>
-                        Home
-                    </Link>
-                </nav>
+    return ( <
+        Router >
+        <
+        div >
+        <
+        nav style = {
+            { padding: "20px", textAlign: "center", background: "#333" } } >
+        <
+        Link to = "/"
+        style = {
+            { color: "#fff", textDecoration: "none", fontWeight: "bold" } } >
+        Home <
+        /Link> <
+        /nav>
 
-                <Routes>
-                    <Route path="/" element={<Posts />} />
-                </Routes>
-            </div>
-        </Router>
+        <
+        Routes >
+        <
+        Route path = "/"
+        element = { < Posts / > }
+        /> <
+        /Routes> <
+        /div> <
+        /Router>
     );
 }
 
